74963d2949a21703f56f84ace4667c409a181129
Matthew Dillard <7395051@gmail.com>
2017-07-08 00:15:38 -0500

    Simple test of functionality
    I'm thinking about adding unit tests to the project, but I'm worried
    that will complicate things for the students.

--- src/Tracer.java
+++ src/Tracer.java
@@ -1,19 +1,45 @@
-/**
- * 
- */
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-/**
- * @author matt
- *
- */
+import javax.imageio.ImageIO;
+
+import Utility.GridTimer;
+import Utility.Vector;
+
 public class Tracer {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GridTimer t = new GridTimer(System.out);
 		
+		t.eventStart("Set up the Scene");
+
+		@SuppressWarnings("unused")
+		Scene s = new Scene();
+
+		t.eventStop("Set up the Scene");
+		t.eventStart("Set up the Camera");
+
+		Camera c = new Camera(new Vector(0, 0, 5), new Vector(1, 0, 0), 400, 200, 8, 5, 90);
+
+		t.eventStop("Set up the Camera");
+
+		c.setTimer(t);
+
+		try {
+			// retrieve image
+			BufferedImage bi = c.testCapture();
+
+			t.eventStart("Writing image to file");
+
+			File outputfile = new File("saved.png");
+			ImageIO.write(bi, "png", outputfile);
+
+			t.eventStop("Writing image to file");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
