ebfb71afdb9b718933ad4848b4e6767925213224
Matthew Dillard <7395051@gmail.com>
2017-07-07 22:35:55 -0500

    Temporary Scene constructor
    Makes a single diffuse sphere on a white floor

--- src/Scene.java
+++ src/Scene.java
@@ -1,7 +1,13 @@
 import java.io.File;
 import java.util.ArrayList;
 
+import Materials.Lambertian;
+import Materials.Material;
+import Shapes.Plane;
 import Shapes.Shape;
+import Shapes.Sphere;
+import Utility.Color;
+import Utility.Vector;
 
 public class Scene {
 	ArrayList<Shape> things;
@@ -9,11 +15,20 @@ public class Scene {
 	
 	public Scene() {
 		refractionIndex = 1.0f;
+		this.things = new ArrayList<Shape>();
+		
 		// TODO: generate a random scene here
+		Material matteWhite = new Lambertian(Color.white());
+		Material matteGreen = new Lambertian(Color.green());
+		
+		things.add(new Plane(matteWhite, new Vector(), new Vector(0,0,1)));
+		things.add(new Sphere(matteGreen, new Vector(5,0,3), 3));
 	}
 	
 	public Scene(File descriptor) {
 		refractionIndex = 1.0f;
+		this.things = new ArrayList<Shape>();
+		
 		// TODO: read a file here
 	}
 }
