a3b44b978bfeec8fe612a9f1ab98d6165ae44b82
Matthew Dillard <7395051@gmail.com>
2017-07-08 21:20:15 -0500

    Implemented Vector inversion, reflection and refraction
    The Vector class is for the most part, complete in its implementation

--- src/utilities/Vector.java
+++ src/utilities/Vector.java
@@ -9,7 +9,7 @@ import java.util.Random;
  * @author mld2443
  */
 public final class Vector {
-	//TODO: determine speed difference if using doubles
+	// TODO: determine speed difference if using doubles
 	public final float x, y, z;
 	private final static Random gen = new Random();
 
@@ -21,9 +21,10 @@ public final class Vector {
 		y = 0.0f;
 		z = 0.0f;
 	}
-	
+
 	/**
 	 * Primary constructor to make a new Vector <x,y,z>
+	 * 
 	 * @param x
 	 * @param y
 	 * @param z
@@ -59,6 +60,16 @@ public final class Vector {
 	////////////////
 	// OPERATIONS //
 	////////////////
+	/**
+	 * Vector inversion/negation. Reverses the direction of the vector without
+	 * changing its magnitude.
+	 * 
+	 * @return Inverted vector
+	 */
+	public Vector invert() {
+		return new Vector(-x, -y, -z);
+	}
+
 	/**
 	 * Vector addition
 	 * 
@@ -144,17 +155,54 @@ public final class Vector {
 	public static Vector cross(final Vector lhs, final Vector rhs) {
 		return new Vector(lhs.y * rhs.z - lhs.z * rhs.y, lhs.z * rhs.x - lhs.x * rhs.z, lhs.x * rhs.y - lhs.y * rhs.x);
 	}
-	
+
+	/**
+	 * A linear algebra reflection of a vector off of a surface.
+	 * 
+	 * @param incoming
+	 *            The direction of the incoming Vector
+	 * @param normal
+	 *            The normal vector of the reflective surface
+	 * @return A vector reflection
+	 */
+	public static Vector reflect(final Vector incoming, final Vector normal) {
+		return sub(incoming, scale(normal, 2.0f * dot(incoming, normal)));
+	}
+
+	/**
+	 * Linear algebra vector refraction. Refracts a vector passing through a
+	 * surface.
+	 * 
+	 * @param incoming
+	 *            The direction of incoming vector
+	 * @param normal
+	 *            Normal vector of the surface
+	 * @param eta
+	 *            The ratios of the indices of refractions
+	 * @return The refracted vector direction
+	 * @see <a href="https://en.wikipedia.org/wiki/Snell%27s_law">Wkipedia:
+	 *      Snell's Law</a>
+	 */
+	public static Vector refract(final Vector incoming, final Vector normal, final float eta) {
+		final float incedent = dot(incoming, normal);
+		final float discriminant = 1.0f - ((eta * eta) * (1.0f - (incedent * incedent)));
+		if (discriminant <= 0) {
+			// Total internal reflection
+			return null;
+		}
+		return sub(scale(sub(incoming, scale(normal, incedent)), eta), scale(normal, (float) Math.sqrt(discriminant)));
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		Vector v = (Vector) obj;
-		
+
 		final float error = v.x + v.y + v.z - x - y - z;
-		
+
 		// build in some small tolerance
 		return error < 0.00001;
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format("<%.2f,%.2f,%.2f>", x, y, z);
