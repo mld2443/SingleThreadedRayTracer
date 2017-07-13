2e874e2b6d73b35206d68a5854a94b4b3a115e55
Matthew Dillard <7395051@gmail.com>
2017-07-13 14:47:29 -0500

    Implemented and documented Lambertian and Metallic material types

--- src/materials/Lambertian.java
+++ src/materials/Lambertian.java
@@ -4,16 +4,29 @@ import utilities.Color;
 import utilities.Ray;
 import utilities.Vector;
 
+/**
+ * A perfectly diffuse, matte material.
+ * 
+ * @author mld2443
+ * @see <a href="https://en.wikipedia.org/wiki/Lambertian_reflectance">Wikipedia: Lambertian Reflactance</a>
+ */
 public class Lambertian extends Material {
 
+	/**
+	 * Constructs a new Lambertian material.
+	 * 
+	 * @param color {@link Color} of our new material
+	 * @see <a href="https://en.wikipedia.org/wiki/Lambertian_reflectance">Wikipedia: Lambertian Reflactance</a> 
+	 */
 	public Lambertian(final Color color) {
 		super(color);
 	}
 
 	@Override
 	public Ray scatter(final Ray incoming, final Vector collision, final Vector normal, final float sceneIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector target = Vector.sum(collision, normal, Vector.randomInUnitSphere());
+        
+        return new Ray(collision, Vector.sub(target, collision));
 	}
 
 }
--- src/materials/Metallic.java
+++ src/materials/Metallic.java
@@ -4,9 +4,24 @@ import utilities.Color;
 import utilities.Ray;
 import utilities.Vector;
 
+/**
+ * A metallic material able to emulate metallic objects with different levels of
+ * polish. Can have a mirror finish or a somewhat opaque look to it.
+ * 
+ * @author mld2443
+ */
 public class Metallic extends Material {
 	private final float fuzz;
 
+	/**
+	 * Construct a Metallic material. Can have a mirror finish, or a fuzziness
+	 * factor.
+	 * 
+	 * @param color
+	 *            The color of this material
+	 * @param fuzz
+	 *            The fuzziness; 0.0 for mirror finish
+	 */
 	public Metallic(final Color color, final float fuzz) {
 		super(color);
 		this.fuzz = fuzz;
@@ -14,8 +29,20 @@ public class Metallic extends Material {
 
 	@Override
 	public Ray scatter(final Ray incoming, final Vector collision, final Vector normal, final float sceneIndex) {
-		// TODO Auto-generated method stub
+		Vector reflected = Vector.reflect(incoming.direction, normal);
+
+		// Apply our fuzziness inside this if block
+		if (fuzz > 0.0f)
+			reflected = Vector.sum(reflected, Vector.randomInUnitSphere().scale(fuzz));
+
+		final Ray bounce = new Ray(collision, reflected);
+
+		// Metallic surfaces have the concept of a front and back side; If the
+		// ray hits the front, it reflects
+		if (Vector.dot(bounce.direction, normal) > 0)
+			return bounce;
+
+		// If the ray hits the back side, it is absorbed.
 		return null;
 	}
-
 }
